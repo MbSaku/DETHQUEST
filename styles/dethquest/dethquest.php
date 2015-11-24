8502f779e40c1e0f3000d476fcf0182c30d9b09f
Marc Mascaro <mbsaku@gmail.com>
2015-11-24 09:25:46 +0100

    15.11.24

---
+++ styles/dethquest/dethquest.php
@@ -0,0 +1,29 @@
+<?php
+switch ($site->getDevice()){
+  case 'mobile':
+    echo '<img id="navbutton" src="'.$site->getBaseroot().'styles/'.$site->getStyle().'/images/mobilemenu.png" onclick="$('."'#navigation'".').slideToggle();">';
+  case 'tablet':
+  default:
+    echo '<a href="'.$site->getBaselink().'/home">
+    <img class="logo" src="'.$site->getBaseroot().'styles/'.$site->getStyle().'/images/gradient.png">
+    </a>';
+}
+?>
+<script>
+window.onload = function() {
+  document.getElementById("content").style.opacity="1";
+  <?php
+  if ($site->getDevice() == 'mobile'){
+   ?>
+   $('#navigation').css('height', (screen.availHeight - $("#header").height()) + "px");
+   $('#navigation').slideToggle(0);
+   <?php
+  }
+  if( $site->getDevice() != 'desktop' ){
+    ?>
+    setHoverables();
+    <?php
+  }
+  ?>
+};
+</script>
